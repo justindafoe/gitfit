f4268b94c60665ea6989580b219f2c1fb43803be
justin <justin.dafoe84@gmail.com>
2021-05-13 08:41:06 -0400

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import blackandredinstagram from '../images/blackandredinstagram.png';
 
 function Header() {
     return (
         <div className="Header">
-          <a href="http://www.instagram.com/grit_f.i.t" target="_blank"><img className="instagram" src="../images/instagram-128-red_.png" alt="instagram" /></a>
+          <a href="http://www.instagram.com/grit_f.i.t" target="_blank"><img className="instagram" src={blackandredinstagram} /></a>
         </div>
     )
 }
