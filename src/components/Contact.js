e4dda59e481b2f5d8de1a995e4c776392ef6493a
justin <justin.dafoe84@gmail.com>
2021-05-14 12:16:10 -0400

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,47 @@
+
+import emailjs from "emailjs-com";
 import React from 'react';
 import './Contact.css';
 
 export default function Contact() {
-    return (
-        <div className="Contact">
-            Contact
+
+    function sendEmail(e) {
+
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_cmmgcoq', e.target, 'user_lg2oUORJCVLQNAfZUaqlB')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+
+    }
+    return(
+        <div>
+            <div className="container">
+            <form onSubmit={sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                        <div className="col-8 form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Tell us about yourself..." name="message"></textarea>
+                        </div>
+                        <div className="col-8 pt-3 mx-auto">
+                            <input type="submit" className="btn btn-info" value="Send Message"></input>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     )
+
 }
